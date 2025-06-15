8b4f5ec787cf5e6a36b730c9eb921f422843bfb4
Vishal Patil <88872900+Vishalpatil0111@users.noreply.github.com>
2025-06-15 12:15:42 +0530

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -84,7 +84,7 @@ const Contact = () => {
             Let's Connect
           </h1>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full max-w-5xl">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 px-6 gap-6 w-full max-w-5xl">
             {contacts.map((contact, index) => (
               <a
                 key={index}
